2a3656b1e0a05e98569fb889545a50bad00ab243
Sergey Yakunin <sergey@yakunins.com>
2026-01-26 14:21:47 -0800

    better install and uninstall

--- install.cmd
+++ install.cmd
@@ -1,27 +1,40 @@
 @echo off
-:: getting startup folder of current user (https://stackoverflow.com/a/68019702)
 setlocal EnableExtensions DisableDelayedExpansion
+
+echo === Language Indicator - Install ===
+echo.
+
+:: Get Windows Startup folder from registry
 set "StartupDir="
-for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
-if not defined StartupDir for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
-if not defined StartupDir set "StartupDir=\"
-if "%StartupDir:~-1%" == "\" set "StartupDir=%StartupDir:~0,-1%"
-if not defined StartupDir set "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-::echo %StartupDir%
+for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Startup 2^>nul') do (
+    if /I "%%I" == "Startup" if not "%%~K" == "" (
+        if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
+    )
+)
+if not defined StartupDir set "StartupDir=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-set CurrentDir=%CD%
+:: Create shortcut in Startup folder
+set "ShortcutName=language-indicator.lnk"
+set "Target=%~dp0language-indicator.exe"
+set "Shortcut=%StartupDir%\%ShortcutName%"
 
-:: create shortcut with Powershell command
-set ShortcutName=language-indicator-shortcut.lnk
-set Target='%CurrentDir%\language-indicator.exe'
-set Shortcut='%StartupDir%\%ShortcutName%'
-set PS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
-%PS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%Shortcut%); $S.TargetPath = %Target%; $S.Save()"
+echo Creating startup shortcut...
+echo   Target: %Target%
+echo   Shortcut: %Shortcut%
+echo.
 
-:prompt
-set /P OPEN=Open startup folder? Y/[N]
-if /I "%OPEN%" neq "Y" goto end
-start shell:startup
-:end
+powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -Command ^
+    "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%Shortcut%'); $s.TargetPath = '%Target%'; $s.WorkingDirectory = '%~dp0'; $s.Save()"
+
+if exist "%Shortcut%" (
+    echo [OK] Shortcut created successfully.
+    echo Language Indicator will start automatically on Windows startup.
+) else (
+    echo [ERROR] Failed to create shortcut.
+)
+
+echo.
+set /P "OPEN=Open startup folder? [Y/N]: "
+if /I "%OPEN%" == "Y" start "" shell:startup
 
 endlocal
--- uninstall.cmd
+++ uninstall.cmd
@@ -1,24 +1,40 @@
-::@echo off
-:: getting startup folder of current user (https://stackoverflow.com/a/68019702)
+@echo off
 setlocal EnableExtensions DisableDelayedExpansion
+
+echo === Language Indicator - Uninstall ===
+echo.
+
+:: Get Windows Startup folder from registry
 set "StartupDir="
-for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
-if not defined StartupDir for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
-if not defined StartupDir set "StartupDir=\"
-if "%StartupDir:~-1%" == "\" set "StartupDir=%StartupDir:~0,-1%"
-if not defined StartupDir set "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-::echo %StartupDir%
+for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Startup 2^>nul') do (
+    if /I "%%I" == "Startup" if not "%%~K" == "" (
+        if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
+    )
+)
+if not defined StartupDir set "StartupDir=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-:: create shortcut with Powershell command
-set ShortcutName=language-indicator-shortcut.lnk
+:: Remove shortcut from Startup folder
+set "ShortcutName=language-indicator.lnk"
+set "Shortcut=%StartupDir%\%ShortcutName%"
 
-del "%StartupDir%\%ShortcutName%"
-echo %ShortcutName% has been removed from startup folder.
+echo Removing startup shortcut...
+echo   Shortcut: %Shortcut%
+echo.
 
-:prompt
-set /P OPEN=Open startup folder? Y/[N]
-if /I "%OPEN%" neq "Y" goto end
-start shell:startup
-:end
+if exist "%Shortcut%" (
+    del "%Shortcut%"
+    if not exist "%Shortcut%" (
+        echo [OK] Shortcut removed successfully.
+        echo Language Indicator will no longer start automatically.
+    ) else (
+        echo [ERROR] Failed to remove shortcut.
+    )
+) else (
+    echo [INFO] Shortcut not found. Nothing to remove.
+)
+
+echo.
+set /P "OPEN=Open startup folder? [Y/N]: "
+if /I "%OPEN%" == "Y" start "" shell:startup
 
 endlocal
